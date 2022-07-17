dec5977b62929d264a3051a775b614abdf433336
Axizeaus <axizeaus@gmail.com>
2022-07-18 01:16:50 +0630

    Intro to react finished

--- part1/part1a/part1/src/App.js
+++ part1/part1a/part1/src/App.js
@@ -1,7 +1,33 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
 
-export default App
+const Footer = () => {
+  return (
+    <div>
+      Learning react with full stack open course
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
